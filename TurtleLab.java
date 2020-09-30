f36b2497243e76916af86c85d2f30d92c7fa3f63
Gokul vipin <gvipin@stu.naperville203.org>
2020-09-30 17:45:07 -0500

    Finished the code to print "Hi!". Created 5 turtles with red, blue and black colors

--- TurtleLab.java
+++ TurtleLab.java
@@ -1,27 +1,13 @@
-
-
-
-
-
-
-
-
 import java.awt.Color;
 /*
  * Name: Turtle lab 
  * Description:This program draws a 2D "Hi!" on the screen using turtle
  * Date:
  */
-
-
 public class TurtleLab
 {
-
     public static void main(String[] args)
-    {      
-
-        
-        
+    {         
         World screen = new World();// creates a canvas on which turtle can crush1 
         Turtle crush1 = new Turtle(screen);// create a new Turtle object from the Turtle class, referenced by the variable 'crush'
         Turtle crush2 = new Turtle(400,240,screen);// create a second Turtle object from the Turtle class, referenced by the variable 'crush'
@@ -33,13 +19,15 @@ public class TurtleLab
         crush2.setPenColor(Color.BLUE);//changes the object to red
         crush3.setPenColor(Color.BLUE);//changes the object to red
         crush4.setPenColor(Color.BLACK);//changes the object to red      
-        crush5.setPenColor(Color.BLACK);//changes the object to red      
+        crush5.setPenColor(Color.BLACK);//changes the object to red  
+        
         //changes the objects' width to 10
         crush1.setWidth(10);
         crush2.setWidth(10);
         crush3.setWidth(10);
         crush4.setWidth(10);
         crush5.setWidth(10);
+        
         //This makes the crush1 object draw a red 2d "H"        
         crush1.forward(75);
         crush1.turnRight();
@@ -64,6 +52,7 @@ public class TurtleLab
         crush1.forward(30);
         crush1.turnRight();
         crush1.forward(20);
+        
         //This makes the crush2 and crush3 object draw a blue 2D "i"
         crush2.forward(55);
         crush2.turnRight();
@@ -79,6 +68,7 @@ public class TurtleLab
         crush3.forward(15);
         crush3.turnRight();
         crush3.forward(20);
+        
         //This makes the crush4 and crush5 object draw a black 2D "!"
         crush4.turnRight();
         crush4.forward(10);
@@ -95,14 +85,6 @@ public class TurtleLab
         crush5.turnRight();
         crush5.forward(10);
         crush5.turnRight();
-        crush5.forward(15);
-        
-        
-        
-        
-        
-       
-       
-        
+        crush5.forward(15); 
     }
 }
