360e12e2fa4cb5cd1f5b8484ef19b18b195e1313
Gokul vipin <gvipin@stu.naperville203.org>
2020-10-02 14:05:11 -0500

    Edited the comments and finished the code

--- TurtleLab.java
+++ TurtleLab.java
@@ -11,15 +11,15 @@ public class TurtleLab
         World screen = new World();// creates a canvas on which turtle can crush1 
         Turtle crush1 = new Turtle(screen);// create a new Turtle object from the Turtle class, referenced by the variable 'crush'
         Turtle crush2 = new Turtle(400,240,screen);// create a second Turtle object from the Turtle class, referenced by the variable 'crush'
-        Turtle crush3 = new Turtle(400,180,screen);// create a second Turtle object from the Turtle class, referenced by the variable 'crush'          
-        Turtle crush4 = new Turtle(430,165,screen);// create a second Turtle object from the Turtle class, referenced by the variable 'crush'                 
-        Turtle crush5 = new Turtle(430,225,screen);// create a second Turtle object from the Turtle class, referenced by the variable 'crush'                 
+        Turtle crush3 = new Turtle(400,180,screen);// create a third Turtle object from the Turtle class, referenced by the variable 'crush'          
+        Turtle crush4 = new Turtle(430,165,screen);// create a fourth Turtle object from the Turtle class, referenced by the variable 'crush'                 
+        Turtle crush5 = new Turtle(430,225,screen);// create a fifth Turtle object from the Turtle class, referenced by the variable 'crush'                 
       
         crush1.setPenColor(Color.RED);//changes the object to red
-        crush2.setPenColor(Color.BLUE);//changes the object to red
-        crush3.setPenColor(Color.BLUE);//changes the object to red
-        crush4.setPenColor(Color.BLACK);//changes the object to red      
-        crush5.setPenColor(Color.BLACK);//changes the object to red  
+        crush2.setPenColor(Color.BLUE);//changes the object to blue
+        crush3.setPenColor(Color.BLUE);//changes the object to blue
+        crush4.setPenColor(Color.BLACK);//changes the object to black      
+        crush5.setPenColor(Color.BLACK);//changes the object to black  
         
         //changes the objects' width to 10
         crush1.setWidth(10);
